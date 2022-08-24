c38ff986fe0b2632d3547bc1c0d6c16757d983ae
Mihir3006 <mylovemummy@live.com>
2022-08-24 20:56:11 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /*Add remote origin*/
 const path = require("path");
 
+const expressLayouts = require("express-ejs-layouts");
+
 const express = require("express");
 
 const port = 5000;
@@ -9,10 +11,13 @@ const app = express();
 
 const homeRouter = require("./routers/index");
 
+app.use(expressLayouts);
 
 app.use("/", homeRouter);
 
 app.set("view engine","ejs");
+
+app.use(express.static('./assets'));
 
 app.set("views", path.join(__dirname,"/views"));
 
